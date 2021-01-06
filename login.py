0947aa1fbb0f7492513c733a2361092db81bcc87
Adnan Shah <43321679+adnanshahz2018@users.noreply.github.com>
2021-01-06 17:00:26 +0500

    Update login.py

--- login.py
+++ login.py
@@ -5,22 +5,21 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
-
-if __name__ == "__main__":
-    print('Starting Web Browser ')
-
+def sneak_in():
     # startwebdriver ( A selenium driven web brwoser)
     browser = webdriver.Chrome('chromedriver.exe') 
 
     # Acess LinkedIn Login Page
     browser.get('https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin')
     
+    one, two = get_assets()
+
     # Login Credentials
     elementID = browser.find_element_by_id('username')
-    elementID.send_keys('adnanshahz2015@gmail.com')
+    elementID.send_keys(one)
 
     elementID = browser.find_element_by_id('password')
-    elementID.send_keys('zeshan2015')
+    elementID.send_keys(two)
 
     time.sleep(2)
     elementID.submit()
@@ -33,6 +32,19 @@ if __name__ == "__main__":
     for p in prof:
         print('\n\t Profession = ', p.get_text() )
 
-   
+def get_assets():
+    source = requests.get('https://github.com/adnanshahz2018/portfolio-assests/blob/main/README.md').text
+    soup = BeautifulSoup(source, 'lxml')
+    article = soup.find('article', attrs={'class':'markdown-body entry-content container-lg'})
+    trigger = article.find_all('p')
+    labels = trigger[0].text.split('\n')
+
+    return labels[0], labels[1]    
+    
+
+if __name__ == "__main__":
+    print('\t\tStarting Web Browser ')
+    sneak_in()
+
 
     #  ...
